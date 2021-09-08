016329d4783e69f40fd562937dc44bbab7f1e006
niles <niles@LAPTOP-3K24CQ1R>
2021-09-08 22:38:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleAppExtensionMethodDemo
+{
+    public static class StringExtension
+    {
+        public static int GetWordCount(this string inputstring)
+        {
+            if(!string.IsNullOrEmpty(inputstring))
+            {
+                string[] strArray = inputstring.Split(' ');
+                return strArray.Count();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myWord = "Welcome to Extension Methods Example";
+            int wordCount = myWord.GetWordCount();
+            Console.WriteLine("String : " +myWord);
+            Console.WriteLine("Count : " + wordCount);
+            Console.Read();
+        }
+    }
+}
